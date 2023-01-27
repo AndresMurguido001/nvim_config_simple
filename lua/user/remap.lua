9309f9786629e3fe170ad3dd31f754966df78dd0
AndresMurguido001 <dre.murguido@gmail.com>
2023-01-27 11:16:12 -0600

    fixed tmux/nvim navigation

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -48,21 +48,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- terminal stuff
 -- assuming youre in a tmux session
--- vim.keymap.set("n", "<C-x>", "<cmd>split | terminal<CR>A", { silent = true })
-
--- Buffer navigation
-vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h")
-vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j")
-vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k")
-vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l")
-vim.keymap.set("i", "<A-h>", "<C-\\><C-N><C-w>h")
-vim.keymap.set("i", "<A-j>", "<C-\\><C-N><C-w>j")
-vim.keymap.set("i", "<A-k>", "<C-\\><C-N><C-w>k")
-vim.keymap.set("i", "<A-l>", "<C-\\><C-N><C-w>l")
-vim.keymap.set("n", "<A-h>", "<C-\\><C-N><C-w>h")
-vim.keymap.set("n", "<A-j>", "<C-\\><C-N><C-w>j")
-vim.keymap.set("n", "<A-k>", "<C-\\><C-N><C-w>k")
-vim.keymap.set("n", "<A-l>", "<C-\\><C-N><C-w>l")
--- Close terminal
-vim.keymap.set("t", "<C-x>", "<C-\\><C-N>:q<CR>")
+vim.keymap.set("n", "<C-h>", "<cmd>lua require(\"tmux\").move_left()<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd>lua require(\"tmux\").move_bottom()<cr>")
+vim.keymap.set("n", "<C-k>", "<cmd>lua require(\"tmux\").move_top()<cr>")
+vim.keymap.set("n", "<C-l>", "<cmd>lua require(\"tmux\").move_right()<cr>")
 

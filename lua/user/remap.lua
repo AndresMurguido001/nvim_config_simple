94eebcd337bf2787d7657640478c25efcfcd4b53
AndresMurguido001 <dre.murguido@gmail.com>
2023-05-13 12:10:08 -0500

    mappings, trouble conf

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<esc>")
 
 -- Move multiple lines in visual mode
